58aeeeaf04df64263485a1e6d7f33769e1664ae0
Tong Zhao <zhaotong0312@outlook.com>
2019-04-07 01:54:35 -0700

    init

---
+++ try_generate_case/try_generate_cases.go
@@ -0,0 +1,51 @@
+package try_generate_case
+
+import (
+"fmt"
+"github.com/labstack/echo"
+"net/http"
+)
+
+func handler(c echo.Context) error {
+	return c.JSON(http.StatusOK, "lol")
+}
+
+
+func generateCases(level, i int, temp []bool, result*[][]bool) {
+	if level == i {
+		res := make([]bool, level)
+		copy(res, temp)
+		*result = append(*result, res)
+	} else {
+		temp[i] = false
+		generateCases(level, i+1, temp, result)
+		temp[i] = true
+		generateCases(level, i+1, temp, result)
+	}
+}
+
+func app(arr []int) {
+	arr = append(arr, 1)
+	arr = append(arr, 1)
+}
+
+func main() {
+	//e := echo.New()
+	//e.POST("/dns/haha", handler)
+	//reqData := []byte("")
+	//
+	//
+	//req := httptest.NewRequest(http.MethodPost, "/dns", bytes.NewReader(reqData))
+	//rec := httptest.NewRecorder()
+	//e.Serv
+	//
+	//eHTTP(rec, req)
+	////c := e.NewContext(req, rec)
+	//
+	//fmt.Println(rec.Body.String())
+	//
+	//
+	result := make([][]bool, 0)
+	generateCases(5,0, make([]bool, 5), &result)
+	fmt.Println(result)
+}
---
+++ try_reader/reader.go
@@ -0,0 +1,5 @@
+package try_reader
+
+import "bytes"
+
+var b = bytes.Buffer{}
